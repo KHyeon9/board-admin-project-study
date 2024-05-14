dac58d71468b909b4d36edccae909803aa391d65
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-05-14 19:05:10 +0900

    first commit

---
+++ src/main/java/com/studyproject/boardadminproject/BoardAdminProjectStudyApplication.java
@@ -0,0 +1,13 @@
+package com.studyproject.boardadminproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardAdminProjectStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardAdminProjectStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyproject/boardadminproject/BoardAdminProjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyproject.boardadminproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardAdminProjectStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
