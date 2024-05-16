9a7318660b62d7a5ff03187d5e95eb8fab8781dc
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-05-16 20:32:22 +0900

    유저 테스트를 위한 data.sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- user account
+insert into user_account (user_id, user_password, role_types, nickname, email, memo, created_at, created_by, modified_at, modified_by)
+values ('hyeon', '{noop}dummy', 'ADMIN', 'Hyeon', 'hyeon@mail.com', 'Hyeon', now(), 'hyeon', now(), 'hyeon'),
+       ('soo', '{noop}dummy', 'MANAGER', 'soo', 'soo@mail.com', 'soo', now(), 'soo', now(), 'soo'),
+       ('kim', '{noop}dummy', 'MANAGER,DEVELOPER', 'kim', 'kim@mail.com', 'kim', now(), 'kim', now(), 'kim'),
+       ('park', '{noop}dummy', 'USER', 'park', 'park@mail.com', 'park', now(), 'park', now(), 'park')
+;
+
