43bcc0abf1f650ed98bb7ec27fefe4877671cac3
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-05-17 21:04:04 +0900

    jpa에 관련하여 db의 값 변경 기능들이 정상인지 확인 하는 코드를 작성

--- src/test/java/com/studyproject/boardadminproject/repository/JpaRepositoryTest.java
+++ src/test/java/com/studyproject/boardadminproject/repository/JpaRepositoryTest.java
@@ -1,16 +1,25 @@
 package com.studyproject.boardadminproject.repository;
 
 import com.studyproject.boardadminproject.domain.UserAccount;
+import com.studyproject.boardadminproject.domain.constant.RoleType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("JPA 연결 테스트")
+@Import(JpaRepositoryTest.TestJpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
 
@@ -33,5 +42,64 @@ class JpaRepositoryTest {
                 .isNotNull()
                 .hasSize(4);
 
+    }
+
+    @DisplayName("회원 정보 insert 테스트")
+    @Test
+    void givenUserAccount_whenInserting_thenWorksFine() {
+        // Given
+        long previousCount = userAccountRepository.count();
+        UserAccount userAccount = UserAccount.of(
+                "test", "pw", Set.of(RoleType.DEVELOPER), null, null, null);
+
+        // When
+        userAccountRepository.save(userAccount);
+
+        // Then
+        assertThat(userAccountRepository.count())
+                .isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("회원 정보 update 테스트")
+    @Test
+    void givenUserAccountAndRoleType_whenUpdating_thenWorksFine() {
+        // Given
+        UserAccount userAccount = userAccountRepository.getReferenceById("hyeon");
+        userAccount.addRoleType(RoleType.DEVELOPER);
+        userAccount.addRoleTypes(List.of(RoleType.USER, RoleType.USER));
+        userAccount.removeRoleType(RoleType.ADMIN);
+
+        // When
+        UserAccount updateAccount = userAccountRepository.saveAndFlush(userAccount);
+
+        // Then
+        assertThat(updateAccount)
+                .hasFieldOrPropertyWithValue("userId", "hyeon")
+                .hasFieldOrPropertyWithValue("roleTypes", Set.of(RoleType.DEVELOPER, RoleType.USER));
+    }
+
+    @DisplayName("회원 정보 delete 테스트")
+    @Test
+    void givenUserAccount_whenDeleting_thenWorksFine() {
+        // Given
+        long previousCount = userAccountRepository.count();
+        UserAccount userAccount = userAccountRepository.getReferenceById("hyeon");
+
+        // When
+        userAccountRepository.delete(userAccount);
+
+        // Then
+        assertThat(userAccountRepository.count())
+                .isEqualTo(previousCount - 1);
+    }
+
+    @EnableJpaAuditing
+    @TestConfiguration // 테스트의 경우에만 스캐닝되어서 사용 -> JpaConfig부분을 치환함
+    static class TestJpaConfig {
+
+        @Bean
+        public AuditorAware<String> auditorAware() {
+            return () -> Optional.of("hyeon");
+        }
     }
 }
