393f2f6d1f53109fc6eec7a783fe016142774ad5
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-05-30 21:00:59 +0900

    Actuator 의 metric 수집을 활용한 방문 숫자 집계 서비스 및 컨트롤러 어드바이스 구현
    전체 컨트롤러에 적용되는 설정이므로 컨트롤러 어드바이스로 접근하고 모든 컨트롤러 테스트가 영향 받으므로 해당 영향을 받게됩니다. 그래서 전역 테스트 설정을 작성하고, 컨트롤러 테스트들에 추가했습니다.

--- src/test/java/com/studyproject/boardadminproject/controller/AdminAccountControllerTest.java
+++ src/test/java/com/studyproject/boardadminproject/controller/AdminAccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.studyproject.boardadminproject.controller;
 
+import com.studyproject.boardadminproject.config.GlobalControllerConfig;
 import com.studyproject.boardadminproject.config.SecurityConfig;
 import com.studyproject.boardadminproject.domain.constant.RoleType;
 import com.studyproject.boardadminproject.dto.AdminAccountDto;
@@ -26,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("컨트롤러 - 어드민 회원 관리")
-@Import(SecurityConfig.class)
+@Import({SecurityConfig.class, GlobalControllerConfig.class})
 @WebMvcTest(AdminAccountController.class)
 class AdminAccountControllerTest {
 
